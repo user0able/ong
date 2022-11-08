9b642bb571901124af6a06e45fab066a42eae17f
Hern4n <hernanfontanez@gmail.com>
2022-11-08 19:16:08 -0300

    feat(mailing): initial app

--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Contact
+from rest_framework import viewsets, permissions
+from .serializers import ContactSerializer
 
-# Create your views here.
+
+class ContactViewSet(viewsets.ModelViewSet):
+    queryset = Contact.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = ContactSerializer
