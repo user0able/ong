a4e6a96cac358c8eb11915862402d66ab32d7009
Hern4n <hernanfontanez@gmail.com>
2022-11-08 19:15:28 -0300

    feat: add initial files

---
+++ README.md
@@ -0,0 +1,14 @@
+# ONG
+
+
+inicializado:
+- endpoint para recibir formularios
+
+required: 
+- python3
+- pip3
+
+optional?:
+- virtualenv 
+- activate virtualenv with requirements.txt
+  - pip install requirements.txt
--- ong/urls.py
+++ ong/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from mailing.views import ContactViewSet as contact_view
+
+router = routers.DefaultRouter()
+router.register(r'contacts', contact_view)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace="rest_framework")),
+    path('', include(router.urls))
 ]
